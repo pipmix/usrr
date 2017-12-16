297a1fa580cc595f278c5c402e3fb2c4e57158bf
pipmix <onloadx@yahoo.com>
2017-12-16 09:36:19 -0600

    Initial commit

---
+++ usrr/models/UserAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIPMIX.models
+{
+	public class UserAccount
+	{
+
+
+
+		public Guid Id { get; protected set; }
+		public string Email { get; protected set; }
+		public string Password { get; protected set; }
+		public string FirstName { get; protected set; }
+		public string LastName { get; protected set; }
+		public string Salt { get; protected set; }
+
+		public DateTime CreationDate { get; protected set; }
+
+		protected UserAccount()
+		{
+		}
+
+		public UserAccount(string firstname, string lastname, string email)
+		{
+			Id = Guid.NewGuid();
+			FirstName = firstname;
+			LastName = lastname;
+			Email = email.ToLowerInvariant();
+			CreationDate = DateTime.UtcNow;
+		}
+
+
+	}
+}
